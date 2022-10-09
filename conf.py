c00ce04ea45da8912a5aedc5b0696f12a17fda34
Bea Shakow <beashakow@gmail.com>
2022-10-09 10:46:59 -0400

    added new conf file for easy globals

---
+++ conf.py
@@ -0,0 +1,12 @@
+##conf.py
+import os
+
+vaultPath = os.path.dirname(__file__)+"\\vault\\"
+meaningsPath = [os.path.dirname(__file__)+"\\meanings\\tarotMeanings.tsv"]
+imgPath = os.path.dirname(__file__)+"\\images\\tarot\\"
+deckPrefsPath = os.path.dirname(__file__)+"\\settings\\deckPrefs.txt"
+
+defaultDeck = "Biddy"
+defaultArt = "Biddy"
+deckChoices = ["Biddy"]
+artChoices = ["Biddy"]
