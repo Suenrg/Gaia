a47421d44c3c681da35a5e6e8b518e15cbbfe196
Bea Shakow <beashakow@gmail.com>
2022-10-09 10:46:40 -0400

    create loadDecks

--- cardDecks.py
+++ cardDecks.py
@@ -1,14 +1,15 @@
+import os
+
 class Card: #create the card class, for storing tarot card objects
     def __init__(self, name, icon, upright, reverse, upLong, revLong):
         self.name = name
         self.icon = icon
         self.upright = upright
         self.reverse = reverse
-        self.upLong = upB
-        self.revLong = revB
+        self.upLong = upLong
+        self.revLong = revLong
     def prints(self):
-        print("Name: " + self.name +" Up: " + self.up + " rev: "+ self.rev + " gen: "+self.upB + ' revB: '+self.revB + ')
-
+        print(f"Name: {self.name}, Up: {self.up}, Rev: {self.rev}, upLong: {self.upLong}, revLong: {self.revLong}")
 
 
 class Deck:
@@ -23,8 +24,25 @@ class Deck:
 
 def loadDecks(paths):
     decks = {}
+    cards = {}
     for i in paths:
+        deckName = ""
         with open(i, errors="ignore") as f:
             for line in f:
-                # create card
+                if (line.startswith("Name")):
+                    deckName=(line.split("\t")[1]).replace("\n", "")
+                else:
+                    line = line.replace("\n", "")
+                    splat = line.split("\t")
+                    newCard = Card(splat[0], splat[1], splat[2], splat[3], splat[4], splat[5])
+                    cards[newCard.name] = newCard
+        decks[deckName] = cards
+
     return decks
+
+# ##main
+# meaningsPath = [os.path.dirname(__file__)+"\\meanings\\tarotMeanings.tsv"]
+# imgPath = os.path.dirname(__file__)+"\\images\\tarot\\"
+#
+# decks = loadDecks(meaningsPath)
+# print(decks)
