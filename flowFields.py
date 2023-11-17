d7f1db52f954c2a59f286b16df956496f138d6e4
Bea Shakow <beashakow@gmail.com>
2023-11-17 12:55:32 -0500

    new file:   flowFields.py

---
+++ flowFields.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import random
+from processing import *
+
+
+canvasSize = PVector(700,700)
+
+def setup():
+    size(canvasSize)
+    background(0)
+
+def draw():
+    ellipse(mouseX, mouseY, 50, 50)
+
+
+
+
+
+#app.exit() # close the windowP
